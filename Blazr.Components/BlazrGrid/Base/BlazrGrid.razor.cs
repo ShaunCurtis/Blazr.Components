97fc17677052ff5ad07dda3d9c85e88fc234b0db
Shaun Curtis <shaun@coldelm.com>
2023-08-31 21:50:49 +0100

    Update

--- Blazr.Components/BlazrGrid/Base/BlazrGrid.razor.cs
+++ Blazr.Components/BlazrGrid/Base/BlazrGrid.razor.cs
@@ -6,23 +6,20 @@
 namespace Blazr.Components.BlazrGrid;
 
 // The Component is configured to get it's data set from one of two sources (in order of precidence):
-//   1. The Parameter provided ListComponentController.
-//   2. The cascaded cascadedListComponentController instance.
+//   1. The Parameter provided ListContext.
+//   2. The cascaded cascadedListContext instance.
 
 public partial class BlazrGrid<TGridItem> : BlazrBaseComponent, IComponent, IHandleEvent, IDisposable
     where TGridItem : class, new()
 {
-    /// <summary>
-    /// One of the three mechanisms for providing 
-    /// </summary>
-    [CascadingParameter] private IListController<TGridItem>? cascadedListComponentController { get; set; }
+    [CascadingParameter] private IBlazrListContext<TGridItem>? cascadedListContext { get; set; }
 
-    [Parameter] public IListController<TGridItem>? ListComponentController { get; set; }
+    [Parameter] public IBlazrListContext<TGridItem>? ListContext { get; set; }
     [Parameter] public RenderFragment? ChildContent { get; set; }
 
-    private IEnumerable<TGridItem> _items = Enumerable.Empty<TGridItem>();
+    private IBlazrListContext<TGridItem>? _listContext { get; set; }
+    private IEnumerable<TGridItem> _items => _listContext?.Items ?? Enumerable.Empty<TGridItem>();
 
-    private IListController<TGridItem>? _listComponentController { get; set; }
     protected readonly List<IBlazrGridColumn<TGridItem>> GridColumns = new();
 
     public void RegisterColumn(IBlazrGridColumn<TGridItem> column)
@@ -39,38 +36,25 @@ public partial class BlazrGrid<TGridItem> : BlazrBaseComponent, IComponent, IHan
         // Set the list Controller, prioritizing the Parameter Value
         if (this.NotInitialized)
         {
-            _listComponentController = ListComponentController ?? cascadedListComponentController;
+            _listContext = ListContext ?? cascadedListContext;
 
-            // Wires up the component to the controller's ListChanged event if it exists
-            if (this._listComponentController is not null)
-            {
-                _listComponentController.StateChanged += OnListChanged;
+            ArgumentNullException.ThrowIfNull(_listContext);
 
-                // If we don't have a pager we need to manually initialise the first data set query
-                if (!_listComponentController.HasPager)
-                {
-                    DummyPager pager = new();
-                    _listComponentController.RegisterPager(pager);
-                    await _listComponentController.NotifyPagingRequestedAsync(pager, new PagingEventArgs { Request = new() });
-                    _listComponentController.RegisterPager(null);
-                }
-            }
+            _listContext.ContextChanged += OnContextChanged;
 
             // Render the component so all the columns can register
             await this.RenderAsync();
         }
 
-        // Sets the internal TGridItem collection
-        _items = _listComponentController ?? Enumerable.Empty<TGridItem>();
-
         // Render the grid content
         this.StateHasChanged();
     }
 
+    //TODO - is this needed
     public async ValueTask RefreshAsync()
         => await this.RenderAsync();
 
-    public async void OnListChanged(object? sender, EventArgs e)
+    public async void OnContextChanged(object? sender, EventArgs e)
         => await this.RenderAsync();
 
     async Task IHandleEvent.HandleEventAsync(EventCallbackWorkItem item, object? obj)
@@ -81,9 +65,7 @@ public partial class BlazrGrid<TGridItem> : BlazrBaseComponent, IComponent, IHan
 
     public void Dispose()
     {
-        if (this._listComponentController is not null)
-        {
-            _listComponentController.StateChanged -= OnListChanged;
-        }
+        if (_listContext is not null)
+            _listContext.ContextChanged += OnContextChanged;
     }
 }
